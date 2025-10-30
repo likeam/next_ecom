66fc23b4cafff35a9faf4792a7a9719c03ecc771
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-10-30 22:21:40 +0500

    Building Admin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["localhost"],
+    formats: ["image/avif", "image/webp"],
+  },
+  env: {
+    SITE_URL: process.env.SITE_URL || "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
